bad9daad956a1d3ac3c5d5361f20a05d1c177920
Bread White <32078281+breadrock1@users.noreply.github.com>
2023-01-07 15:04:41 +0300

    Updated main.rs file after all changes.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-// Метка todo - реализовать самостоятельно
-
-// ***** Пример библиотеки "Умный дом" со статическим содержимым
-
 use std::collections::HashMap;
-use std::fmt::format;
-use std::hash::Hash;
-use std::ops::Add;
+
+mod devices;
+mod house;
+
+use devices::providers::{OwningDeviceInfoProvider, BorrowingDeviceInfoProvider};
+use devices::sockets::{SmartSocket, SmartThermometer};
+use house::premises::{Room, SmartHouse};
 
 #[macro_export]
 macro_rules! dict {
@@ -18,164 +18,48 @@ macro_rules! dict {
     };
 }
 
-
-struct Device {
-    id: String
-}
-
-impl Device {
-    fn new(name: &String) -> Self {
-        Device(name)
-    }
-}
-
-
-struct Room {
-    name: String,
-    devices: HashMap<String, String>
-}
-
-impl Room {
-    fn empty() -> Self {
-        Room()
-    }
-
-    fn new(name: &String, devices: &HashMap<String, String>) -> Self {
-        Room(name, devices)
-    }
-}
-
-
-struct SmartHouse {
-    address: String,
-    rooms: HashMap<String, Room>
-}
-
-impl SmartHouse {
-    fn empty() -> Self {
-        SmartHouse()
-    }
-
-    fn new(address: &String, rooms: &HashMap<String, Room>) -> Self {
-        SmartHouse(address, rooms)
-    }
-
-    fn get_rooms(&self) -> &HashMap<String, Room> {
-        &self.rooms
-    }
-
-    fn devices(&self, room: &str) -> &HashMap<String, String> {
-        &self.get_rooms()
-            .get(room)
-            .expect("Unknown room name!")
-            .devices
-    }
-
-
-    fn create_report(&self, provider: &dyn DeviceInfoProvider) -> String {
-        self.get_rooms()
-            .into_iter()
-            .map(|tuple_| tuple_.1)
-            .map(|room_| self.room_devices_status(&room_, provider))
-            .collect();
-
-        return String::from("");
-    }
-
-    fn room_devices_status(&self, room: &Room, provider: &dyn DeviceInfoProvider) -> String {
-        room.devices
-            .values()
-            .map(|device_| provider.device_status(&room.name, device_))
-            .reduce(|first, second| first.add(&second));
-
-        return String::from("");
-    }
-}
-
-// ***** Пример использования библиотеки умный дом:
-
-// Пользовательские устройства:
-struct SmartSocket(Device);
-struct SmartThermometer(Device);
-
-// Пользовательские поставщики информации об устройствах.
-// Могут как хранить устройства, так и заимствывать.
-struct OwningDeviceInfoProvider {
-    socket: SmartSocket,
-}
-struct BorrowingDeviceInfoProvider<'a, 'b> {
-    socket: &'a SmartSocket,
-    thermo: &'b SmartThermometer,
-}
-
-trait DeviceInfoProvider {
-    fn device_status(&self, room: &str, device: &dyn Device) -> String;
-}
-
-// todo: реализация трейта `DeviceInfoProvider` для поставщиков информации
-impl<T> DeviceInfoProvider for T {
-    fn device_status(&self, room: &str, device: &str) -> String {
-        format!("{} room: device {} -> enabled", room, device)
-    }
-}
-
-impl DeviceInfoProvider for BorrowingDeviceInfoProvider {
-    fn device_status(&self, room: &str, device: &BorrowingDeviceInfoProvider) -> String {
-        format!("{} room: device {} -> {}", room, device., device.thermo.0)
-    }
-}
-
-
 fn main() {
+    // Инициализация устройств
+    let socket1 = SmartSocket {
+        id: "1".to_string(),
+    };
+    let socket2 = SmartSocket {
+        id: "2".to_string(),
+    };
+    let thermometer = SmartThermometer {
+        id: "3".to_string(),
+        temperature: 24.0,
+    };
 
-    let mut house = SmartHouse::new(
-        &String::from("Moscow, Arbatskay 10"),
-        &dict!(
-
-            "kitchen".to_string() => Room::new(
-                &"kitchen".to_string(),
-                dict!(
-                    "" => Device::new("".to_string()),
-                )
+    // Инициализация дома
+    let house = SmartHouse::new(
+        "Moscow, Arbatskaya, 10, 1".to_string(),
+        dict!(
+            "Bedroom".to_string() => Room::new(
+                String::from("Bedroom"),
+                vec![socket1.id.clone()]
             ),
 
-            "bedroom".to_string() => Room::new(
-                &"bedroom".to_string(),
-                dict!(
-                    "" => Device::new("".to_string()),
-                )
-            ),
-
-            "bathroom".to_string() => Room::new(
-                &"bathroom".to_string(),
-                dict!(
-                    "" => Device::new("".to_string()),
-                )
+            "Kitchen".to_string() => Room::new(
+                "Kitchen".to_string(),
+                vec![
+                    socket2.id.clone(),
+                    thermometer.id.clone()
+                ]
             )
-        )
+        ),
     );
 
-
-    // Инициализация устройств
-    let socket1 = SmartSocket { 0: Device { id: "".to_string() } };
-    let socket2 = SmartSocket { 0: Device { id: "".to_string() } };
-    let thermo = SmartThermometer { 0: Device { id: "".to_string() } };
-
-
     // Строим отчёт с использованием `OwningDeviceInfoProvider`.
-    let info_provider_1 = OwningDeviceInfoProvider {
-        socket: socket1,
-    };
-    // todo: после добавления обобщённого аргумента в метод, расскоментировать передачу параметра
-    let report1 = house.create_report(/* &info_provider_1 */);
+    let info_provider_1 = OwningDeviceInfoProvider { socket: socket1 };
+    let report1 = house.create_report(&info_provider_1);
 
     // Строим отчёт с использованием `BorrowingDeviceInfoProvider`.
     let info_provider_2 = BorrowingDeviceInfoProvider {
         socket: &socket2,
-        thermo: &thermo,
+        thermometer: &thermometer,
     };
-    // todo: после добавления обобщённого аргумента в метод, расскоментировать передачу параметра
-    let report2 = house.create_report(/* &info_provider_2 */);
+    let report2 = house.create_report(&info_provider_2);
 
     // Выводим отчёты на экран:
     println!("Report #1: {report1}");
