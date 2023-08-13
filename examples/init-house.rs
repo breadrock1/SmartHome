6246466435c9cd22a11aa7c21f8530bec4a7c92b
Bread White <breadrock1@gmail.com>
2023-08-13 12:35:05 +0300

    Update examples after all changes

--- examples/init-house.rs
+++ examples/init-house.rs
@@ -1,21 +1,22 @@
 extern crate smarthome;
 
-use smarthome::sockets::smart_sockets::SocketType;
 use crate::smarthome::*;
+use smarthome::sockets::smart_sockets::SocketType;
 
 fn main() -> Result<(), anyhow::Error> {
-    let kitchen_socket = SmartSocket::new("kitchen_socket".to_string());
-    let kitchen_therm = SmartThermometer::new("kitchen_thermometer".to_string());
+    let kitchen_socket = SmartSocket::new("kitchen_socket");
+    let kitchen_therm = SmartThermometer::new("kitchen_thermometer");
     let devices: Vec<SocketType> = vec![
         SocketType::from(kitchen_socket.clone()),
         SocketType::from(kitchen_therm.clone()),
     ];
-    let mut kitchen_room = Room::new("kitchen".to_string()).unwrap();
+    let mut kitchen_room = Room::new("kitchen");
     kitchen_room.add_devices(devices).unwrap();
 
-    let new_therm = SmartSocket::new("new_socket".to_string());
-    let new_therm = SocketType::from(new_therm);
-    kitchen_room.add_device(new_therm).unwrap();
+    let thermometer = SmartSocket::new("new_socket");
+    kitchen_room
+        .add_device(SocketType::from(thermometer))
+        .unwrap();
 
     let mut house = SmartHouse::new("Moscow");
     house.add_room(kitchen_room).unwrap();
@@ -24,7 +25,7 @@ fn main() -> Result<(), anyhow::Error> {
         socket: &kitchen_socket,
         thermometer: &kitchen_therm,
     };
-    let report_result = house.create_report(&info_provider);
-    println!("{}", report_result);
+
+    println!("{}", house.create_report(&info_provider));
     Ok(())
 }
